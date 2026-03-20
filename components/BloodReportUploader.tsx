a16d8d368c69dbab160420dfc972711a8a6a1535
Cyrus Amalan <cyrus.amalan@gmail.com>
2026-03-19 23:28:33 -0700

    update bug

--- components/BloodReportUploader.tsx
+++ components/BloodReportUploader.tsx
@@ -10,7 +10,7 @@ interface BloodReportUploaderProps {
 
 async function extractTextFromPDF(file: File): Promise<string> {
   const pdfjsLib = await import('pdfjs-dist');
-  pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.mjs`;
+  pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.mjs';
 
   const arrayBuffer = await file.arrayBuffer();
   const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
